841673b01789ff0e0ba2a7a7aec8e2492c7cc02a
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-07-16 23:09:03 +0200

    Added kotlin-frontend and java-backend modules

---
+++ java-spring-backend/src/main/java/eu/groeller/javaaspringbackend/JavaSpringBackend.java
@@ -0,0 +1,13 @@
+package eu.groeller.javaaspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBackend {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBackend.class, args);
+    }
+
+}
---
+++ java-spring-backend/src/test/java/eu/groeller/javaaspringbackend/JavaSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.groeller.javaaspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
