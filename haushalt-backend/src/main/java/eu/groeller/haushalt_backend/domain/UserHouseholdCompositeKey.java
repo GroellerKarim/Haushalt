e49467ff0bc698e8f0c008159d0a951092bc935e
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-07-24 14:20:02 +0200

    Added UserHousehold Composite Key

---
+++ haushalt-backend/src/main/java/eu/groeller/haushalt_backend/domain/UserHouseholdCompositeKey.java
@@ -0,0 +1,33 @@
+package eu.groeller.haushalt_backend.domain;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Embeddable
+public class UserHouseholdCompositeKey  {
+
+    private UUID householdId;
+    private UUID userId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserHouseholdCompositeKey that)) return false;
+        return Objects.equals(householdId, that.householdId) && Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(householdId, userId);
+    }
+}
